312a5b296e97f1d3dd733d56b7a7a7a2e85231bf
aliyasser20 <aliyasser20@hotmail.com>
2020-04-13 10:49:55 -0400

    format spots text output

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -9,10 +9,21 @@ const dayListItem = props => {
     "day-list__item--full": props.spots === 0
   });
 
+  const formatSpots = spots => {
+    switch (spots) {
+      case 0:
+        return "no spots remaining";
+      case 1:
+        return "1 spot remaining";
+      default:
+        return `${spots} spots remaining`;
+    }
+  };
+
   return (
     <li className={dayClass} onClick={() => props.setDay(props.name)}>
       <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light">{props.spots}</h3>
+      <h3 className="text--light">{formatSpots(props.spots)}</h3>
     </li>
   );
 };
