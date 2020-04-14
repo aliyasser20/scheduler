bf48040fac83752b7d5b0e5276f1e756d5f83f21
aliyasser20 <aliyasser20@hotmail.com>
2020-04-14 11:38:17 -0400

    appointment component update

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -1,7 +1,16 @@
 import React from "react";
 
+import Header from "./Header";
+import Show from "./Show";
+import Empty from "./Empty";
+
 import "./styles.scss";
 
-const appointment = props => <article className="appointment"></article>;
+const appointment = props => (
+  <article className="appointment">
+    <Header time={props.time} />
+    {props.interview ? <Show {...props.interview} /> : <Empty />}
+  </article>
+);
 
 export default appointment;
